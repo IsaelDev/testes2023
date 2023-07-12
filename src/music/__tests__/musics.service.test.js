755f1438b421005d35b8e6a17fcadbfde2ee1511
Isaelson Gutembergh <isaelsongutembergh@gmail.com>
2023-07-11 21:41:13 -0300

    Implementando testes unitarios mocks

--- src/music/__tests__/musics.service.test.js
+++ src/music/__tests__/musics.service.test.js
@@ -151,4 +151,100 @@ describe('music servic', ()=>{
             musicsService.add(music)
         }).toThrowError()
     })
+
+    //teste 11
+    test('Testa se push é chamado corretamente', () => {
+      const musics = [];
+      const music = 'Some Music';
+      addMusic(music);
+    
+      expect(musics.push).toHaveBeenCalledWith(music);
+      expect(musics.length).toBe(1);
+    });
+    
+    //teste 12
+    test('Testa se irá retornar a música correta quando encontrada', () => {
+    const musics = [
+    { title: 'Smooth Criminal', artist: 'Michael Jackson' },
+    { title: 'Billie Jean', artist: 'Michael Jackson' },
+    { title: 'Beat It', artist: 'Michael Jackson' },
+    ];
+
+    const title = 'Beat It';
+    const result = searchMusic(title);
+
+    expect(result).toEqual({ title: 'Beat It', artist: 'Michael Jackson' });
+});
+
+    //teste13
+    test('Testa se retorna undefined quando a música não é encontrada', () => {
+    const musics = [
+    { title: 'One Dance', artist: 'Drake' },
+    { title: 'Meu pedaço de pecado', artist: 'João Gomes' },
+    { title: 'Acordando o predio', artist: 'Luan Santana' },
+    ];
+
+    const title = 'One Dance';
+    const result = searchMusic(title);
+
+    expect(result).toBeUndefined();
+});
+    //teste14
+    test('Testa se retorna undefined quando o título fornecido é uma string vazia', () => {
+    const musics = [
+    { title: 'Se for amor', artist: 'João Gomes' },
+    { title: 'Eu tenho a senha', artist: 'João Gomes' },
+    { title: 'Dengo', artist: 'João Gomes' },
+    ];
+
+    const title = '';
+    const result = searchMusic(title);
+
+    expect(result).toBeUndefined();
+});
+    //tete15
+    test('Testa se removerá corretamente quando há 2 músicas com o mesmo título', () => {
+    const musics = [
+    { title: 'One Dance', artist: 'Drake' },
+    { title: 'One Dance', artist: 'Michael' },
+    { title: 'Dengo', artist: 'João Gomes' },
+    ];
+  
+    const musicToRemove = { title: 'One Dance', artist: 'Michael' };
+    removeMusic(musicToRemove);
+  
+    expect(musics).toEqual([
+      { title: 'One Dance', artist: 'Drake' },
+      { title: 'Dengo', artist: 'João Gomes' },
+    ]);
+  });
+  
+  //teste16 
+  test('Testa se removerá corretamente a primeira música do array', () => {
+    const musics = [
+      { title: 'One Dance', artist: 'Michael' },
+      { title: 'One Dance', artist: 'Drake' },
+      { title: 'Dengo', artist: 'João Gomes' },
+    ];
+  
+    const musicToRemove = { title: 'One Dance', artist: 'Michael' };
+    removeMusic(musicToRemove);
+  
+    expect(musics).toEqual([
+      { title: 'One Dance', artist: 'Drake' },
+      { title: 'Dengo', artist: 'João Gomes' },
+    ]);
+  });
+  //teste17
+  test('Testa se removerá corretamente quando há apenas uma música no array', () => {
+    const musics = [{ title: 'One Dance', artist: 'Drake' }];
+    const musicToRemove = { title: 'One Dance', artist: 'Drake' };
+    removeMusic(musicToRemove);
+  
+    expect(musics).toEqual([]);
+  });
+  //teste18
+  
+
 })
+
